4fae92e3421d39795bfc546405cec35d698f8639
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-19 18:06:10 -0500

    initial implementation for wordsearch, pair programming with Adam Schulte

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,7 +20,7 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -34,5 +34,23 @@ describe("#wordSearch()", function() {
     ], 'SEINFELD')
 
     assert.isTrue(result);
+
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['A', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['P', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['P', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['Y', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'HAPPY')
+
+    assert.isTrue(result);
+
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,54 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+    //creating a row and joining the letters
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+    
+
+    if (search(horizontalJoin, word)) {
+        return true;
+    };
+    if (search(verticalJoin, word)) {
+        return true;
+    };
+    return false;
+};
+
+const search = function(arrayOfWords, word) {
+    for (let row of arrayOfWords) {
+        if (row.includes(word)) {
+            return true
+        };
+    };
+    return false    
 }
+
+const transpose = function(matrix) {
+    let newMatrix = [];
+    const outerLength = matrix.length;
+    const innerLength = matrix[0].length;
+   
+    // transposing
+    for (let i = 0; i < innerLength; i++) {
+      let innerArray = [];
+      for (let j = 0; j < outerLength; j++) {
+        innerArray.push(matrix[j][i]);
+      }
+      newMatrix.push(innerArray);
+    }
+    return newMatrix;
+};
+
+wordSearch([
+    ['A', 'H', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'P', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'P', 'V', 'R', 'G'],
+    ['A', 'H', 'C', 'S', 'Y', 'Y', 'R', 'L'],
+    ['P', 'F', 'R', 'E', 'N', 'E', 'B', 'B'],
+    ['P', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+    ['Y', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'HAPPY');
 
 module.exports = wordSearch
